abc539f2f0f6c5e70f3124a7a96967326b5c7196
mDestaFadilah <desta.08b@gmail.com>
2023-08-26 17:29:36 +0700

    https://packagist.org/packages/maatwebsite/excel

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+
     ],
 
     /*
@@ -229,6 +231,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
